d2b0d03533cefaef57269fc309df2d4d0b10c30a
Oscar <oscardelicaat@gmail.com>
2021-01-17 21:04:39 +0100

    cleanup in app.js and added thing for background audio:

--- App.js
+++ App.js
@@ -13,11 +13,6 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import RootNavigator from './src/navigation/RootNavigator';
 
-import { createStackNavigator } from '@react-navigation/stack';
-
-
-import SaveRecordingScreen from './src/views/SaveRecordingScreen';
-const Stack = createStackNavigator();
 
 
 
@@ -25,10 +20,6 @@ const App: () => React$Node = () => {
   return (
     <NavigationContainer>
       <RootNavigator />
-{/* 
-      <Stack.Navigator>
-        <Stack.Screen name="SaveRecording" component={SaveRecordingScreen} />
-      </Stack.Navigator> */}
     </NavigationContainer>
   );
 };
